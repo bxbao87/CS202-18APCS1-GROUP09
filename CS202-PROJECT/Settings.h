63ae1f5b159173b0a18accd71a8086b1b83066de
Le Viet Thanh <alphatester1819@gmail.com>
2019-11-18 22:17:07 +0700

    Update Settings.h

--- CS202-PROJECT/Settings.h
+++ CS202-PROJECT/Settings.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <Windows.h>
+#include <conio.h>
+#include <time.h>
+#include <thread>
+
+void FixConsoleWindow() {
+	HWND consoleWindow = GetConsoleWindow();
+	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+	SetWindowLong(consoleWindow, GWL_STYLE, style);
+}
+
+void go(int column, int line)
+{
+	COORD coord;
+	coord.X = column;
+	coord.Y = line;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void color(const int& k)
+{
+	HANDLE  hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, k);
+}
+
