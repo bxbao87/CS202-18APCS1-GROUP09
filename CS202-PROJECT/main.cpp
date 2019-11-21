6f5716036e0a6c0eceb7f5722276d87137dc23e8
bxbao87 <bxbao18@apcs.vn>
2019-11-21 14:53:47 +0700

    testing

---
+++ CS202-PROJECT/main.cpp
@@ -0,0 +1,40 @@
+#include "thanh.h"
+#include"People.h"
+
+void exitThread(thread* t,LEVEL& a)
+{
+	a.kill();
+	t->join();
+}
+
+int main()
+{
+	/*system("chcp 437");
+	system("cls");
+	DRAW screen;
+	
+	screen.split();
+	
+	LEVEL test(1);
+	People p;
+	int k = 0;
+	thread t(&LEVEL::run, &test);
+	while (k != 27)
+	{
+		k = _getch();
+		if (k == 27)
+		{
+			exitThread(&t, test);
+		}
+		else if (k == 'p')
+		{
+			test.pause();
+		}
+		else if (k == 'r')
+		{
+			test.resume();
+		}
+	}*/
+	system("pause");
+	return 0;
+}
